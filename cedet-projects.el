0d2fe619cd72fed8a9951f27634afd058bd7acc4
fagle <ifagle@qq.com>
2016-07-22 11:06:37 +0800

    local changes

---
+++ cedet-projects.el
@@ -0,0 +1,13 @@
+(ede-cpp-root-project "project_root"
+                      :file "/media/sf_MengLegend_Mobile/branches/Rebuild_June/Makefile"
+                      :include-path '("/Lib/libbson-1.0.2/include"
+                                      "/Server"
+                                      "/Server/RoleServer"
+                                      "/Server/Common"
+                                      "/Server/LoginSvr"
+                                      "/Server/DBService"
+                                      "/Server/DBService/libmongoc-1.0"
+                                      "/Server/DBService/libbson-1.0") ;; add more include
+                      ;; paths here
+                                        ; :system-include-path '("~/linux")
+                      )
